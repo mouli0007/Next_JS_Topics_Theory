1c5acfdc190614e36c84f2f7c56dfe5be53394e9
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-03 21:51:15 +0530

    Create getStaticPaths.js

---
+++ getStaticPaths.js
@@ -0,0 +1,53 @@
+ ########## SYNTAX ###############
+		  
+		 export const getStaticPaths = async (context) => {
+		 
+  // We have to specify which instance of dynamic page should be generated
+  // those below id with page data will be pre generated
+           
+		    export const getStaticPaths = async (context) => {
+  // We have to specify which instance of dynamic page should be generated
+  // those below id with page data will be pre generated
+
+           return {
+                paths: [
+                          { params: { pid: "p1" } },
+                          { params: { pid: "p2" } },
+                          { params: { pid: "p3" } },
+                       ],
+                           fallback: false,
+                         };
+                        };
+						
+	// Fallback: false
+               
+			   This keyword is used when you want to dynamically 
+			   pre render a lots of pages with different id
+			   
+			   
+
+     	// Fallback: true 
+		              
+			   To return a pre-generated page with high priority
+			   and to pre render only some pages instaead of pre rendering N[number] of pages
+         
+		 // Fallback : 'blocking'
+		          
+				  it will wait to the non pre rendered page to be generated 
+				  so we dont need to add any kind of if checking
+				  
+				  export const getStaticPaths = async (context) => {
+                        // We have to specify which instance of dynamic page should be generated
+                         // those below id with page data will be pre generated
+
+                const data = get_data();
+                 const ids = data.map((product) => product.id);
+
+                const params = ids.map((id) => ({ params: { pid: id } }));
+
+                 return {
+                    paths: params,
+ 
+                   fallback: false,
+                      };
+                     };
