1dbd3f314ed7b337250fb0106678502ce10b8bae
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-13 23:51:17 +0530

    Create NextJS_13.js

---
+++ NextJS_13.js
@@ -0,0 +1,14 @@
+Next js 13 Inshights !
+
+### App Directory ### All components are server somponents not client components
+
+1=> We can create an app directory and make it as an main Index page rather than using index.js in pages folder
+2=> It will create an automatic layout section which we can avoid creating with manual components 
+3=> and we can make file routing same as we does in pages directory
+4=> In the Link Component we dont need to use a tag to work on different scenarios
+
+5=> Since its a server components we can make it as async even in high level component
+
+6=> For dynamic pages we have to create a dynamic folder with a square bracket and we have to add a page.js file 
+    so that file will act as a dynamic page
+ 
