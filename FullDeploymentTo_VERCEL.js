41474ea9bdeb53344d41d8b0a9491ab46d76020f
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-15 19:26:47 +0530

    Create FullDeploymentTo_VERCEL.js

---
+++ FullDeploymentTo_VERCEL.js
@@ -0,0 +1,8 @@
+  Commands : 
+        
+		=> npm run build
+		=> npm start (to run our local production server )
+		=> it runs on localhost:3000 
+
+   #### Push your latest code to github 
+	 #### and if any code changes in your repository it will redploy again in VERCEL
