f57c3c62e23c1e19a362be74e934813eb0066494
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-15 22:46:08 +0530

    Create NextJS_AUTHentication.js

---
+++ NextJS_AUTHentication.js
@@ -0,0 +1,30 @@
+############################################
+		######### Adding Authentication ############
+		
+		
+		#### Adding User Authentication With Next JS
+		
+		#### How Authentication Work ..? 
+		
+		1=> We will be entering a data from client side 
+		2=> request for user credentials to the server
+		3=> and we get a response (yes/no)
+		5=> In order to avoid any fake response we ne to use two things ! 
+		
+	         1=> Server Side Sessions
+			 
+			     Store the unique indentifer on server , send same identifier to client
+			      Client Sends Identifier along with request to protected route
+				  
+			 2=> Authentication tokens
+			 
+			      it creates (but not store) permission token on server, send token to client
+				  client sends token along with the request to protected routes
+				  it cannot be accessble by javascript to prevent cross-site scripting attacks
+				  
+				  ######################################
+				  ######### (JWT) Tokens Logic ##########
+				  
+				  
+			1=> Servers dont save information about clinet authenticated clients
+			2=> Instead, clients should get information that allows them to prove their authentication
