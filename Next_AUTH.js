82f7600dff079613bd8f81d6c25fbd746bcadedb
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-15 23:38:32 +0530

    Create Next_AUTH.js

---
+++ Next_AUTH.js
@@ -0,0 +1,10 @@
+
+	##############################
+			#############  Next AUTH #####################
+			
+			
+			NextAuth.js is a complete open-source authentication solution for Next.js applications. 
+        It is designed from the ground up to support Next.js and Serverless.10-Feb-2023
+
+        1=> it provides both server and client side validations! 
+		   2=> it also provvides auth tokens for validations
