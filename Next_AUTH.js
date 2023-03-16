4587c8fc27f0bf50b4c69ebf9b2048e330cd59e4
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-16 21:54:33 +0530

    Update Next_AUTH.js

--- Next_AUTH.js
+++ Next_AUTH.js
@@ -7,4 +7,52 @@
         It is designed from the ground up to support Next.js and Serverless.10-Feb-2023
 
         1=> it provides both server and client side validations! 
-		   2=> it also provvides auth tokens for validations
+        2=> it also provvides auth tokens for validations
+
+		   
+		   ######### Syntax #########
+		   
+		   
+		   export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      //  authorize method is called when it recives an incomming login request
+      async authorize(credentials) {
+        // We are checking with our datase base
+        const client = await connectToDatabase();
+
+        const usersCollection = client.db().collection("users");
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+
+          throw new Error("No User Found");
+        }
+
+        // It returns a Boolean
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Could Not Log You in ");
+        }
+
+        //  We return a object if everytihng is ok and then we encode
+        //this in JSON web token
+        client.close();
+        return {
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
